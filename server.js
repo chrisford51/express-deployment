abde2bda9eb0a58409280f90acc9a44800fbc9f9
Chris Ford <chrisford51@gmail.com>
2018-07-07 03:45:30 -0700

    added handlebars and new home/about routes

--- server.js
+++ server.js
@@ -1,25 +1,41 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-//SET ROUTE
+// //SET ROUTE
+// app.get('/', (req, res) => {
+//     //res.send('Hello Express!');
+//     res.send({
+//         name: 'Chris',
+//         likes: [
+//             'guitar',
+//             'racecars'
+//         ]
+//     });
+// });
+
+// HOME ROUTE
 app.get('/', (req, res) => {
-    //res.send('Hello Express!');
-    res.send({
-        name: 'Chris',
-        likes: [
-            'guitar',
-            'racecars'
-        ]
+    res.render('index.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome!'
     });
 });
 
+// ABOUT ROUTE
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
+// BAD LINK ERROR ROUTE
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Error handling request.'
